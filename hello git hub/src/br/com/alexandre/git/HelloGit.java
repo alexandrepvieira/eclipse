df7e58f9c8257477d32cb62a8dfbea03f50b4215
alexandrepvieira <30581305+alexandrepvieira@users.noreply.github.com>
2019-01-13 13:57:53 -0200

    adicionado arquivos via upload

---
+++ hello git hub/src/br/com/alexandre/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.alexandre.git;
+
+/**
+ * @author giant
+ *teste de integração do eclipse com GitHub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
